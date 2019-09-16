0a65bec136b65387c62e36ea7688f6b41855202e
Newforce39 <justin.warden@outlook.com>
2019-09-16 14:51:11 -0400

    completed save and print button

--- src/scripts/events/eventsdomprinter.js
+++ src/scripts/events/eventsdomprinter.js
@@ -6,7 +6,8 @@ import jwapiManager from "./eventsapimanager.js"
 const jweventsdomprinter = {
     buildeventcomponent: () => {
         const eventLog = document.querySelector("#event-section")
-
+        eventLog.innerHTML= ""
+        document.querySelector("#apisection").innerHTML = ""
         eventLog.innerHTML = `
 <h2>EVENTS</h2>
 <button id="event-button">Add Event</button>
@@ -14,9 +15,9 @@ const jweventsdomprinter = {
         jwapiManager.getallevents()
             .then(parsedevents => {
                 // When they come back from the API, print them
-                parsedevents.forEach(singleEvent => {
-                    document.querySelector("#apisection").innerHTML +=
-                        `<p>${singleEvent.name}</p>
+            parsedevents.forEach(singleEvent => {
+            document.querySelector("#apisection").innerHTML +=
+         `<p>${singleEvent.name}</p>
         <p>${singleEvent.location}</p>
         <p>${singleEvent.date}</p>`
                 })
@@ -25,24 +26,3 @@ const jweventsdomprinter = {
 }
 
 export default jweventsdomprinter;
-
-        //     document
-        //  .querySelector("#jwsubmitbutton")
-        //  .addEventListener("click", function() {
-        //    // On click, create an object with the values from the user's inputs
-
-        //    const newEventToPost = {
-        //      nameOfEvent: document.querySelector("#nameofevent").value,
-        //      locationOfEvent: document.querySelector("#locationofevent").value,
-        //      dateOfEvent: document.querySelector("#dateofevent").value
-        //    };
-        //    // Post the object to json-server
-
-        //    jwapiManager
-        //      .then(parsedEventEntryArray => {
-        //        // Get all the journal entries from json-server and reprint them to refresh the page
-        //        jweventsdomprinter.getallevents(parsedEventEntryArray);
-        //      })
-        //      .postevent(newEventToPost)
-        //      .then(jwapiManager.getallevents)
-        //     }
