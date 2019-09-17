b40db87dc6fcabe3acda4d4ce00cac85d7450bda
kelli-evans09 <kelli.evans09@yahoo.com>
2019-09-17 13:25:40 -0400

    login and register functions work properly

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+// import formPrinter from "../scripts/login/printToDom.js";
+import loginManager from "./login/loginManager.js";
+
+
+// // formPrinter.printLoginForm();
+// // formPrinter.printRegisterForm();
+loginManager.loggingIN();
+loginManager.register();
+loginManager.logOut();
+
